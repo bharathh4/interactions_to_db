612d8ded826cdff80fe7f6f7844c04b2036d1b9c
Bharath <bharathh4@gmail.com>
2016-03-15 12:52:11 -0500

    added conf mean, varaiance methods to analyse module. Should switch to numpy

--- datamanager/analyse.py
+++ datamanager/analyse.py
@@ -1,15 +1,26 @@
 import dbhelper
 import orm_helper
-import os
+import os, math
 from constants import DATA_DIR, DB_PATH
 
 def get_conf_avg(storename='AUS'):
     Transcriptions = orm_helper.Transcriptions
-    confs = [row.conf for row in Transcriptions.select().where(Transcriptions.storename == 'AUS')]
-    return sum(confs)
+    confs = [row.conf for row in Transcriptions.select().where(Transcriptions.store == storename)]
+    return sum(confs)/float(len(confs))
+    
+def get_conf_variance(storename='AUS'):
+    Transcriptions = orm_helper.Transcriptions
+    mean = get_conf_avg(storename=storename)
+    diff_sqs = [(row.conf - mean) * (row.conf - mean) for row in Transcriptions.select().where(Transcriptions.store == storename)]
+    return sum(diff_sqs)/float(len(diff_sqs))
+    
+def get_sample_row():
+    Transcriptions = orm_helper.Transcriptions
+    rows = [row for row in Transcriptions.select()]
+    return rows[0]
     
 def main():
-    get_conf_avg(storename='AUS')
+    print math.sqrt(get_conf_variance(storename='AUS'))
     
 if __name__ == '__main__':
     main()
